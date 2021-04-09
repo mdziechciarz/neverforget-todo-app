39c3d557d0f4f345e6491e5141f8d298339cde68
Michal Dziechciarz <michal.dziechciarz1@gmail.com>
2021-04-09 18:12:46 +0200

    feat: Add mobile version of hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Header />
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import heroIllustration from '../../assets/hero.png';
+
+import style from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <img className={style.img} src={heroIllustration} alt="" />
+      <div className={style.wrapper}>
+        <div className={style.heading}>
+          <h1>Get your life<br /><span>organized</span></h1>
+          <p>“The man who moves a mountain begins by carrying away small stones.” - Confucius</p>
+          <Link className={style.primaryCta} to={{
+            pathname: "/auth",
+            state: {
+              loginView: false
+            }
+          }}>Get started</Link>
+        </div>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header

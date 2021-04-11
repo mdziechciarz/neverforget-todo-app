8596344c2c4f4f6f8931d701fc16ba69c9685407
Michal Dziechciarz <michal.dziechciarz1@gmail.com>
2021-04-11 21:17:35 +0200

    Create info section

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import InfoSection from './components/InfoSection/InfoSection';
 import Navbar from './components/Navbar/Navbar';
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Header />
+      <InfoSection />
     </BrowserRouter>
   )
 }
---
+++ client/src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import customCategories from '../../assets/custom_categories.png';
+import dataProtection from '../../assets/data_protection.png';
+import notifications from '../../assets/notifications.png';
+import personalStatistics from '../../assets/personal_statistics.png';
+import realTimeSync from '../../assets/real_time_sync.png';
+import taskPriorities from '../../assets/task_priorities.png';
+
+import style from './InfoSection.module.css';
+
+const InfoSection = () => {
+  return (
+    <section className={style.infoSection}>
+      <div className={style.info}>
+        <p className={style.logo}>Never<span>Forget</span></p>
+        <p className={style.description}>
+          NeverForget is a simple todo app to designed to help you increase your productivity and achieve your goals.
+        </p>
+      </div>
+      <div className={style.graphics}>
+        {/* <div className={style.wrapper}> */}
+        <div className={style.container}>
+          <img className={style.img} src={personalStatistics} alt="Personal Statistics" />
+          <p className={style.title}>Personal Statistics</p>
+        </div>
+        <div className={style.container}>
+          <img className={style.img} src={taskPriorities} alt="Task Priorities" />
+          <p className={style.title}>Task Priorities</p>
+        </div>
+        <div className={style.container}>
+          <img className={style.img} src={customCategories} alt="Custom Categories" />
+          <p className={style.title}>Custom Categories</p>
+        </div>
+        <div className={style.container}>
+          <img className={style.img} src={notifications} alt="Notifications" />
+          <p className={style.title}>Notifications</p>
+        </div>
+        <div className={style.container}>
+          <img className={style.img} src={realTimeSync} alt="Real Time Sync" />
+          <p className={style.title}>Real Time Sync</p>
+        </div>
+        <div className={style.container}>
+          <img className={style.img} src={dataProtection} alt="Data Protection" />
+          <p className={style.title}>Data Protection</p>
+        </div>
+      </div>
+      {/* </div> */}
+    </section>
+  )
+}
+export default InfoSection

a5684e388eec553010acf9d735bf624c1bdfbb58
Michal Dziechciarz <michal.dziechciarz1@gmail.com>
2021-05-12 09:15:58 +0200

    refactor: change header's illustrations paths

--- client/src/components/Main/TasksPanel/TasksPanel.jsx
+++ client/src/components/Main/TasksPanel/TasksPanel.jsx
@@ -14,15 +14,16 @@ const TasksPanel = () => {
   return (
     <div className={style.container}>
       <Category name="All" openedByDefault>
-        <Task name="Make your homework" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" deadline={Date.now() + 7 * 24 * 60 * 60 * 1000} priority={2} />
-        <Task name="Make your homework and make the business calls" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" deadline={Date.now() + 7 * 24 * 60 * 60 * 1000} priority={2} />
+        <Task name="Do your homework" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" deadline={Date.now() + 7 * 24 * 60 * 60 * 1000} priority={2} />
+        <Task name="Do your homework and make the business calls" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" deadline={Date.now() + 7 * 24 * 60 * 60 * 1000} priority={2} />
         <Task name="Clean the kitchen and take out the rubbish" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" priority={1} />
         <Task name="Take dog for a walk" />
       </Category>
       <Category name="School">
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
+        <Task name="Do your homework" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" deadline={Date.now() + 7 * 24 * 60 * 60 * 1000} priority={2} />
+        <Task name="Do your homework and make the business calls" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" deadline={Date.now() + 7 * 24 * 60 * 60 * 1000} priority={2} />
+        <Task name="Clean the kitchen and take out the rubbish" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" priority={1} />
+        <Task name="Take dog for a walk" />
       </Category>
       <div className={style.buttonsContainer}>
         <button className={style.addTaskButton}><span>+</span>Add Task</button>
